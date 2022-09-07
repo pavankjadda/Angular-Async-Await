8b40dcc9a43b283f02403b114f80f618391e7730
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-09-06 22:17:12 -0400

    chore(deps): updates angular to version 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
-import {HelloComponent} from './hello.component';
 import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
@@ -15,8 +14,7 @@ import {HttpClientModule} from "@angular/common/http";
         ],
     declarations:
         [
-            AppComponent,
-            HelloComponent
+            AppComponent
         ],
     bootstrap:
         [AppComponent]
